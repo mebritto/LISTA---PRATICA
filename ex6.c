bd2cf025f71a904c8ec8b65cec6851e756d26757
maria eduarda <mariaeduardadebritto12345@gmail.com>
2024-09-09 10:50:25 -0300

    exercicio 6.c

--- ex6.c
+++ ex6.c
@@ -19,33 +19,33 @@ int main(){
     int X1;
     int X2;
 
-    printf("digite os primeiros 9 digitos do seu CPF, separando-os por espaços: ");
+    printf("digite os primeiros 9 digitos do seu CPF, separando-os por espaÃ§os: ");
     scanf("%d %d %d %d %d %d %d %d %d", &a, &b, &c, &d, &e, &f, &g, &h, &i);
 
 
     X1 = 10*a + 9*b + 8*c + 7*d + 6*e + 5*f + 4*g + 3*h + 2*i;
         r1 = X1 % 11;
 
-        if((r1>2)&&(r1<=9)){
+        if((r1>2)&&(r1<=10)){
             p = 11 - r1;
-            printf("seu primeiro digito de verificação é: %d\n", p);
+            printf("seu primeiro digito de verificaÃ§Ã£o Ã©: %d\n", p);
                     }
                 else{
                     p = 0;
-                    printf("seu primeiro digito de verificação é: %d\n", p);
+                    printf("seu primeiro digito de verificaÃ§Ã£o Ã©: %d\n", p);
                 }
 
     X2 = 11*a + 10*b + 9*c + 8*d + 7*e + 6*f + 5*g + 4*h + 3*i + 2*p;
         r2 = X2 % 11;
 
 
-        if((r2>2)&&(r2<=9)){
+        if((r2>2)&&(r2<=10)){
             q = 11 - r2;
-            printf("seu primeiro digito de verificação é: %d\n", q);
+            printf("seu primeiro digito de verificaÃ§Ã£o Ã©: %d\n", q);
                     }
                 else{
                     q = 0;
-                    printf("seu primeiro digito de verificação é: %d\n", q);
+                    printf("seu primeiro digito de verificaÃ§Ã£o Ã©: %d\n", q);
                 }
 
 return 0;
